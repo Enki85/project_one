42a5d736a440a14af60fe84b8818e82efe5552e4
enki85 <enki85@gmail.com>
2019-01-06 23:54:30 +0100

    adressadded

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_06_224425) do
+ActiveRecord::Schema.define(version: 2019_01_06_225217) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "stars"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
